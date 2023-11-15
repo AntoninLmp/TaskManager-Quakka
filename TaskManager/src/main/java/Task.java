32149c729026e41972b2ea1eb91d5b2b588bb652
Antonin Lmp <73228586+AntoninLmp@users.noreply.github.com>
2023-11-15 15:44:41 +0100

    Feat: Task prod

---
+++ TaskManager/src/main/java/Task.java
@@ -0,0 +1,35 @@
+public class Task {
+    private static int lastId = 0;
+    private final int id;
+    private final String description;
+    private String status;
+
+    public Task(String description) {
+        this.description = description;
+        this.id = lastId++;
+        this.status = " ";
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getStatus() {
+        return status;
+    }
+
+    public void markAsDone() {
+        this.status = "done";
+    }
+
+    public void markAsToDo() {
+        this.status = "todo";
+    }
+
+    @Override
+    public String toString() {
+        return id + " [" + (status.equals("done") ? "x" : " ") + "] " + description;
+    }
+}
