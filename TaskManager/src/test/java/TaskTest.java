95b7866c0dcce7a5ed1d95f851f36def59a31f2a
nelson <nelson.amorim.branco@efrei.net>
2023-11-15 16:07:16 +0100

    [Feature] Added Task tests

--- TaskManager/src/test/java/TaskTest.java
+++ TaskManager/src/test/java/TaskTest.java
@@ -1,6 +1,6 @@
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class TaskTest {
 
@@ -10,17 +10,34 @@ public class TaskTest {
         task.markAsDone();
         assertEquals(task.getStatus(),"done");
     }
-
     @Test
     public void statusIsToDo(){
         Task task = new Task("Test");
         task.markAsToDo();
         assertEquals(task.getStatus(),"todo");
     }
-
     @Test
     public void statusIsEmptyWhenItIsCreated (){
         Task task = new Task("Test");
         assertEquals(task.getStatus()," ");
+    }
+
+    @Test
+    public void descriptionIsNotEmpty(){
+        Task task = new Task("Test");
+        assertNotNull(task.getDescription());
+    }
+    @Test
+    public void descriptionIsGood(){
+        String maDescription = "Test";
+        Task task = new Task(maDescription);
+        assertEquals(task.getDescription(),maDescription);
+    }
+
+    @Test
+    public void idIsIncrementing() {
+        Task firstTask = new Task("task1");
+        Task secondTask = new Task("task2");
+        assertEquals(secondTask.getId(), firstTask.getId() + 1);
     }
 }
